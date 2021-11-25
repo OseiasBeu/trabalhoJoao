6c043b7f27e11bc40264bfab07eaf80ebfcfd17c
Oseias Beu <beuoseias@gmail.com>
2021-11-25 20:08:32 -0300

    Add files via upload

--- game_functions.py
+++ game_functions.py
@@ -1,27 +1,38 @@
 import sys
 import pygame
 from words import Word
+from text_input import TextInputBox
 
-def check_events(ai_settings,screen,text_input_box_group):
+def check_events(ai_settings,screen,textinput):
     """ Responde a eventos de pressionamento de teclas e de mouse. """
-    for event in pygame.event.get():
+    # Entrada de texto
+    # font = pygame.font.SysFont(None, 100)
+    # text_input_box = TextInputBox(50, 50, 400, font)
+    # group = pygame.sprite.Group(text_input_box)
+    event_list = pygame.event.get()
+    for event in event_list:
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_settings, screen,text_input_box_group)
+            check_keydown_events(event, ai_settings, screen,textinput)
+            textinput.update(event_list)
+    
 
 
 
-def check_keydown_events(event, ai_settings, screen, text_input_box_group):
+def check_keydown_events(event, ai_settings, screen, textinput):
     """ Responde a pressionamento de teclas."""
     if event.key == pygame.K_q:
         sys.exit()
-    else:
-        text_input_box_group.update(pygame.event.get())
+    if event.key == pygame.K_RETURN:
+        print(f'{textinput.value}')
+        textinput.value = ''
+    # else:
+    #     text_input_box_group.update(pygame.event.get())
 
 
     
-def update_screen(ai_settings, screen,list_words, gun,text_input_box_group):
+def update_screen(ai_settings, screen,list_words, gun, textinput):
     """ Atualiza as imagens da tela."""
     #------ Inicia Musicas
     ai_settings.sound
@@ -35,7 +46,9 @@ def update_screen(ai_settings, screen,list_words, gun,text_input_box_group):
         screen.blit(ai_settings.fonte.render(word['palavra'], True,(0,0,0)),(word['x'],word['y']))
     gun.blitme()
 
-    text_input_box_group.draw(screen)
+    screen.blit(textinput.surface, (ai_settings.screen_width/4, ai_settings.screen_height-100))
+
+    # text_input_box_group.draw(screen)
     pygame.display.flip()
 
 
--- jogo.py
+++ jogo.py
@@ -2,13 +2,14 @@ import pygame
 from settings import Settings
 import game_functions as gf
 from gun import Gun
-from text_input import TextInputBox
+import pygame_textinput
+
 def run_game():
     """ Classe responsável por executar o jogo"""
     pygame.init()
     ai_settings = Settings()
 
-    #Criando tela priincipal
+    #Criando tela principal
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Shot word!")
 
@@ -17,18 +18,13 @@ def run_game():
 
     # Cria lista Palavras
     list_words = gf.create_list_word(ai_settings,screen)
-    
-    # Entrada de texto
-    font = pygame.font.SysFont(None, 100)
-    text_input_box = TextInputBox(50, 50, 400, font)
-    text_input_box_group = pygame.sprite.Group(text_input_box)
 
-
+    # Create TextInput-object
+    textinput = pygame_textinput.TextInputVisualizer()
+ 
     """ Criar classe nível"""
-
     # ===== Loop principal =====
     while True:
-        gf.check_events(ai_settings,screen,text_input_box_group)
-        # group.update(event_list)
-        gf.update_screen(ai_settings,screen,list_words, gun,text_input_box_group)
+        gf.check_events(ai_settings,screen, textinput)
+        gf.update_screen(ai_settings,screen,list_words, gun,textinput)
 run_game()
